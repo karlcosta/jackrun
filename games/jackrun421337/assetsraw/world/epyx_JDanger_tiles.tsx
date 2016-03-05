c2e60765d977fa4947c4df7ca6f64015bd1c0683
Jeremias Trautmann <jeremias@tt-sw.com>
2016-03-05 04:08:21 +0100

    Switched to tile based map and other character graphics.

---
+++ games/jackrun421337/assetsraw/world/epyx_JDanger_tiles.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="epyx_JDanger_tiles" tilewidth="32" tileheight="32" tilecount="400" columns="25">
+ <image source="epyx_JDanger_tiles.png" trans="000000" width="800" height="512"/>
+ <terraintypes>
+  <terrain name="Wand" tile="108"/>
+  <terrain name="Boden" tile="142"/>
+ </terraintypes>
+ <tile id="13" terrain="1,1,1,"/>
+ <tile id="14" terrain="1,1,,"/>
+ <tile id="15" terrain="1,1,,1"/>
+ <tile id="16" terrain="1,,,"/>
+ <tile id="17" terrain=",1,,"/>
+ <tile id="38" terrain="1,,1,"/>
+ <tile id="40" terrain=",1,,1"/>
+ <tile id="41" terrain=",,1,"/>
+ <tile id="42" terrain=",,,1"/>
+ <tile id="59" terrain=",,0,0"/>
+ <tile id="63" terrain="1,,1,1"/>
+ <tile id="64" terrain=",,1,1"/>
+ <tile id="65" terrain=",1,1,1"/>
+ <tile id="84" terrain=",,0,0"/>
+ <tile id="109" terrain="0,0,0,0"/>
+ <tile id="116" terrain=",,,1"/>
+ <tile id="117" terrain=",,1,1"/>
+ <tile id="118" terrain=",,1,"/>
+ <tile id="133" terrain="0,0,0,0"/>
+ <tile id="134" terrain="0,0,0,0"/>
+ <tile id="141" terrain=",1,,1"/>
+ <tile id="143" terrain="1,,1,"/>
+ <tile id="166" terrain=",1,,"/>
+ <tile id="167" terrain="1,1,,"/>
+ <tile id="168" terrain="1,,,"/>
+</tileset>
--- games/jackrun421337/game.js
+++ games/jackrun421337/game.js
@@ -43,36 +43,10 @@ JackDanger.JackRun421337.prototype.preload = function() {
 	logInfo("preload JackRun");
 	this.load.path = 'games/' + currentGameData.id + '/assets/';//nicht anfassen
 	
-    //füge hie rein was du alles laden musst.
+    //füge hier ein was du alles laden musst.
     this.load.atlas("jack");
-	this.load.atlas("world");
-	this.map = {};
-	this.map.rows = 10;
-	this.map.data = [0,0,0,0,0,0,0,0,0,0,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1,
-					 1,1,1,1,1,1,1,1,1,1];
+	this.load.tilemap("map", "map.json", null, Phaser.Tilemap.TILED_JSON);
+	this.load.image("tiles", "epyx_JDanger_tiles.png");
 }
 
 //wird nach dem laden gestartet
@@ -82,11 +56,16 @@ JackDanger.JackRun421337.prototype.create = function() {
     Pad.init();//nicht anfassen
     removeLoadingScreen();//nicht anfassen
 	
+	this.physics.startSystem(Phaser.Physics.ARCADE);
+	
 	this.stage.backgroundColor = 0x000000;
-	this.world = new JackDanger.JackRun421337.World(this, this.map);
-	this.jack  = new JackDanger.JackRun421337.Jack(this);
-
-    this.addStuff();
+	
+	this.speed = 200;
+	this.timeText = game.add.bitmapText(game.width / 2, 20, "testfont", "", 30);
+	this.timeText.anchor.set(0.5);
+	
+	this.world = new JackDanger.JackRun421337.World(this);
+	this.jack  = new JackDanger.JackRun421337.Jack(this, this.speed);
 }
 
 //wird jeden Frame aufgerufen
@@ -95,12 +74,7 @@ JackDanger.JackRun421337.prototype.update = function() {
 	
     var dt = this.time.physicsElapsedMS * 0.001;
 
-	this.world.move(this.speed * dt);
-    /*this.playerControls(dt);
-    this.updateBall(dt);
-    this.bounding();
-    this.collision();
-    this.updateTime(dt);*/
+	this.jack.update();
 }
 
 /////////////////////////////////////////////////////////
@@ -109,87 +83,55 @@ JackDanger.JackRun421337.prototype.update = function() {
 /////////////////////////////////////////////////////////
 
 /**
- * Klasse JackRun421337
- */
-JackDanger.JackRun421337.prototype.addStuff = function(dt) {
-	this.worldOffset = 0;
-	this.wallOffset = 0;
-	
-	this.speed = 200;
-
-	this.timeText = game.add.bitmapText(game.width / 2, 20, "testfont", "", 30);
-	this.timeText.anchor.set(0.5);
-	
-}
-
-JackDanger.JackRun421337.prototype.playerControls = function(dt) {
-	if (Pad.isDown(Pad.UP)) {
-		this.jack.moveUp(this.speed * dt);
-	}
-
-	if (Pad.isDown(Pad.DOWN)) {
-		this.jack.moveDown(this.speed * dt);
-	}
-}
-
-/**
  *	Klasse Jack
  */
-JackDanger.JackRun421337.Jack = function(game) {
+JackDanger.JackRun421337.Jack = function(game, speed) {
 	logInfo("generate Jack")
-	this.sprite = game.add.sprite(50,50, "jack", "jack1.png");
-	this.sprite.scale.setTo(0.2, 0.2);
+	this.sprite = game.add.sprite(50,50, "jack", "jack_rechts0");
+	
+	game.physics.arcade.enable(this.sprite);
+	
+	this.sprite.body.collideWorldBounds = true;
+	this.sprite.body.velocity.x = speed;
+	
+	game.camera.follow(this.sprite);
+	
+	this.speed = speed;
+	
 	this.setAnimations();
-	this.doAnimation("run");
+	this.doAnimation("right");
 }
 
 JackDanger.JackRun421337.Jack.prototype = {
 	setAnimations: function() {
-		// TODO
-		this.sprite.animations.add("run", ["jack1.png", "jack2.png", "jack1.png", "jack0.png"], 12, true, false);
+		this.sprite.animations.add("right", ["jack_rechts0", "jack_rechts1", "jack_rechts2"], 12, true, false);
 	},
 	
 	doAnimation: function(name) {
-		// TODO
-		//this.sprite.animations.play(name);
+		this.sprite.animations.play(name);
 	},
 	
-	moveUp: function(dist) {
-		this.sprite.y -= dist;
-	},
-	
-	moveDown: function(dist) {
-		this.sprite.y += dist;
+	update: function() {
+		this.sprite.body.velocity.y = 0;
+		if (Pad.isDown(Pad.UP)) {
+			this.sprite.body.velocity.y = -this.speed;
+		}
+
+		if (Pad.isDown(Pad.DOWN)) {
+			this.sprite.body.velocity.y = this.speed;
+		}
 	}
 }
 
 /**
  * Klase World
  */
-JackDanger.JackRun421337.World = function(game, map) {
+JackDanger.JackRun421337.World = function(game) {
 	logInfo("generate world");
-	// generate sprites
-	this.sprites = [];
-	for (var i = 0; i < map.data.length; i++) {
-		var positionX = Math.floor(i / map.rows) * game.game.height / map.rows;
-		var positionY = (i % map.rows) * game.game.height / map.rows;
-		var sprite = null;
-		if (map.data[i] == 1)
-			sprite = game.add.sprite(positionX, positionY, "world", "ground.png");
-		
-		if (sprite != null) {
-			var scale = game.game.height / (map.rows * sprite.height);
-			sprite.scale.setTo(scale, scale);
-			this.sprites.push(sprite);
-		}
-	}
-}
- 
-JackDanger.JackRun421337.World.prototype = {
-	move: function(dist) {
-		for (var i = 0; i < this.sprites.length; i++) {
-			var sprite = this.sprites[i];
-			sprite.x -= dist;
-		}
-	}
+	
+	this.camera = game.camera;
+	this.map = game.add.tilemap("map");
+	this.map.addTilesetImage("epyx_JDanger_tiles", "tiles");
+	this.layer = this.map.createLayer("Kachelebene 1", 800, 450);
+	this.layer.resizeWorld();
 }
