72cffb7bbb0c2a7e0d173d3c173e12cb681a70d4
Jeremias Trautmann <jeremias@tt-sw.com>
2016-03-09 01:13:14 +0100

    Added speed up.

--- games/jackrun421337/game.js
+++ games/jackrun421337/game.js
@@ -62,11 +62,14 @@ JackDanger.JackRun421337.prototype.create = function() {
 	
 	this.physics.startSystem(Phaser.Physics.ARCADE);
 	
+	this.allowedNumbers = [0, 1, 2, 3, 4, 5, 7, 8, 11, 13, 16, 17, 19, 23, 29, 31, 32, 37, 41, 42, 43, 47, 53, 59, 61, 64, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 128, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 256, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 512, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1024, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1337, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999];
+	
 	this.stage.backgroundColor = 0x000000;
 	this.windowWidth = game.width;
 	this.windowHeight = game.height;
 	this.lose = false;
 	this.speed = 200;
+	this.number = -1;
 	
 	this.jackOffset = 64;
 	var jackPos = {};
@@ -76,6 +79,7 @@ JackDanger.JackRun421337.prototype.create = function() {
 	this.world = new JackDanger.JackRun421337.World(this);
 	this.spikes = new JackDanger.JackRun421337.Spikes(this, this.world, this.speed);
 	this.jack  = new JackDanger.JackRun421337.Jack(this, jackPos, this.speed);
+	this.hud = new JackDanger.JackRun421337.HUD(this);
 	
 	this.world.map.setTileIndexCallback([67,68,92,93], this.collectDiamond, this);
 	
@@ -86,6 +90,7 @@ JackDanger.JackRun421337.prototype.create = function() {
 JackDanger.JackRun421337.prototype.update = function() {
 	var dt = this.time.physicsElapsedMS * 0.001;
 	
+	
 	if (this.lose) {
 		if (this.loseCounter == 0) {
 			onLose();
@@ -93,24 +98,59 @@ JackDanger.JackRun421337.prototype.update = function() {
 		this.loseCounter--;
 	}
 	else {
+		// Spieler-Steuerung
+		this.playerControls();
+		
 		// Kamera bewegen
 		this.camera.x = this.jack.position.x - this.jackOffset;		
+		
+		// Kollisions-Erkennung
 		this.physics.arcade.collide(this.jack.sprite, this.world.layer);
 		this.physics.arcade.overlap(this.jack.sprite, this.spikes.sprites, this.spikesOverlap, null, this);
 	}
 	
 	// Jack bewegen
-	this.jack.update();
+	this.jack.update(dt);
 }
 
 JackDanger.JackRun421337.prototype.render = function() {
 	//game.debug.body(this.jack.sprite);
 }
 
+JackDanger.JackRun421337.prototype.playerControls = function() {
+	// Bewegung y-Achse
+	if (Pad.isDown(Pad.UP)) {
+		this.jack.moveUp();
+	}
+	else if (Pad.isDown(Pad.DOWN)) {
+		this.jack.moveDown();
+	}
+	else {
+		this.jack.stopMove();
+	}
+	
+	// Beschleunigung x-Achse
+	if (Pad.justDown(Pad.RIGHT)) {
+		if (this.number != -1) {
+			if (this.allowedNumbers.indexOf(this.number) != -1) {
+				this.jack.speedUp(2, 1);
+				this.hud.goodNumber();
+			}
+			else {
+				this.jack.speedUp(0.5, 1);
+				this.hud.badNumber();
+			}
+			
+			this.number != -1;
+		}
+	}
+}
+
 JackDanger.JackRun421337.prototype.collectDiamond = function(sprite, tile) {
 	if (sprite === this.jack.sprite) {
-		var number = this.randomIntFromInterval(1,2000);
-		this.world.collectDiamond(tile, number);
+		this.number = this.randomIntFromInterval(0,2000);
+		this.world.collectDiamond(tile, this.number);
+		this.hud.setNumber(this.number);
 	}
 }
 
@@ -134,6 +174,8 @@ JackDanger.JackRun421337.Jack = function(game, position, speed) {
 	
 	this.position = position;
 	this.speed = speed;
+	this.speedFactor = 1;
+	this.factorDuration = 0;
 	this.kill = false;
 	this.killFrame = false;
 	
@@ -173,18 +215,10 @@ JackDanger.JackRun421337.Jack.prototype = {
 		this.sprite.animations.stop();
 	},
 	
-	update: function() {
+	update: function(dt) {
 		if (!this.kill) {
 			// Bewegung
-			this.sprite.body.velocity.x = this.speed;
-			this.sprite.body.velocity.y = 0;
-			if (Pad.isDown(Pad.UP)) {
-				this.sprite.body.velocity.y = -this.speed;
-			}
-
-			if (Pad.isDown(Pad.DOWN)) {
-				this.sprite.body.velocity.y = this.speed;
-			}
+			this.sprite.body.velocity.x = this.speed * this.speedFactor;
 			this.position.x = this.sprite.x;
 			this.position.y = this.sprite.y;			
 		}
@@ -198,6 +232,31 @@ JackDanger.JackRun421337.Jack.prototype = {
 			}
 			this.killFrame = !this.killFrame;
 		}
+
+		if (this.speedFactor != 1) {
+			logInfo("factorDuration: " + this.factorDuration);
+			this.factorDuration -= dt;
+			if (this.factorDuration < 0) {
+				this.speedFactor = 1;
+			}
+		}
+	},
+	
+	speedUp: function(factor, duration) {
+		this.speedFactor *= factor;
+		this.factorDuration = duration;
+	},
+	
+	moveUp: function() {
+		this.sprite.body.velocity.y = -this.speed * this.speedFactor;
+	},
+	
+	moveDown: function() {
+		this.sprite.body.velocity.y = this.speed * this.speedFactor;
+	},
+	
+	stopMove: function() {
+		this.sprite.body.velocity.y = 0;
 	}
 }
 
@@ -241,7 +300,7 @@ JackDanger.JackRun421337.World.prototype = {
 		numberText.body.velocity.y = -this.numberTextsSpeed;
 	},
 	
-	replaceTile(tile) {
+	replaceTile: function(tile) {
 		this.map.replace(tile.index, 143, tile.x, tile.y, 1, 1, this.layer);
 	},
 	
@@ -278,9 +337,33 @@ JackDanger.JackRun421337.Spikes = function(game, world, speed) {
 }
 
 JackDanger.JackRun421337.Spikes.prototype = {
-	stop() {
+	stop: function() {
 		for (var i = 0; i < this.sprites.children.length; i++) {
 			this.sprites.children[i].body.velocity.x = 0;
 		}
+	}
+}
+
+/**
+ * HUD
+ */
+JackDanger.JackRun421337.HUD = function(game) {
+	this.numberText = game.add.bitmapText(game.windowWidth / 2, 0, "testfont", "", 32);
+	this.numberText.anchor.setTo(0.5, 0);
+	this.numberText.fixedToCamera = true;
+}
+
+JackDanger.JackRun421337.HUD.prototype = {
+	setNumber(number) {
+		this.numberText.tint = 0xffffff;
+		this.numberText.text = number.toString();
+	},
+	
+	goodNumber() {
+		this.numberText.tint = 0x00ff00;
+	},
+	
+	badNumber() {
+		this.numberText.tint = 0xff0000;
 	}
 }
