3d32998214fa0e922a5b9e4690630dbdf5649513
Jeremias Trautmann <jeremias@tt-sw.com>
2016-03-16 22:41:29 +0100

    Updated framework.

--- js/loadingscreen.js
+++ js/loadingscreen.js
@@ -2,25 +2,28 @@ JackDanger.LoadingScreen = function(gameState, skip){
 	this.gameState = gameState;
 	this.skip = skip;
 
-	this.back = gameState.add.sprite(0,0, "loadingback");
+	this.hud = gameState.add.group();
+	this.hud.fixedToCamera = true;
+
+	this.back = gameState.add.sprite(0,0, "loadingback", null, this.hud);	
+	gameState.world.setBounds(0,0,800,450);
 	
-	
-	this.loadingText = gameState.add.bitmapText(gameState.world.width / 2 + 50, gameState.world.height - 40, "bigYellow", "0%", 30);
+	this.loadingText = gameState.add.bitmapText(gameState.world.width / 2 + 50, gameState.world.height - 40, "bigYellow", "0%", 30, this.hud);
 	this.loadingText.anchor.set(0.5);
 
-	this.gameNameText = gameState.add.bitmapText(gameState.world.width / 2 - 110, 145, "bigYellow", currentGameData.name, 60);
+	this.gameNameText = gameState.add.bitmapText(gameState.world.width / 2 - 110, 145, "bigYellow", currentGameData.name, 60, this.hud);
 
 
-	this.devText = gameState.add.bitmapText(gameState.world.width / 2 - 110, 210, "white", "von " + currentGameData.developerName, 20);
+	this.devText = gameState.add.bitmapText(gameState.world.width / 2 - 110, 210, "white", "von " + currentGameData.developerName, 20, this.hud);
 
-	this.adviceText = gameState.add.bitmapText(gameState.world.width / 2 + 100, 40, "white", currentGameData.tutorialText, 25);
+	this.adviceText = gameState.add.bitmapText(gameState.world.width / 2 + 100, 40, "white", "" + currentGameData.tutorialText, 25, this.hud);
 	this.adviceText.tint = 0x323457;
 	this.adviceText.anchor.set(0.5);
 
-	this.controlsText = gameState.add.bitmapText(gameState.world.width / 2  - 20, 296, "white", currentGameData.cursorText, 20);
-	this.jumpText = gameState.add.bitmapText(gameState.world.width / 2  + 180, 272, "white", currentGameData.jumpText, 20);
-	this.shootText = gameState.add.bitmapText(gameState.world.width / 2  + 180, 318, "white", currentGameData.shootText, 20);
-	
+	this.controlsText = gameState.add.bitmapText(gameState.world.width / 2  - 20, 296, "white", "" + currentGameData.cursorText, 20, this.hud);
+	this.jumpText = gameState.add.bitmapText(gameState.world.width / 2  + 180, 272, "white", "" + currentGameData.jumpText, 20, this.hud);
+	this.shootText = gameState.add.bitmapText(gameState.world.width / 2  + 180, 318, "white", "" + currentGameData.shootText, 20, this.hud);
+
 
 }
 
@@ -53,16 +56,8 @@ JackDanger.LoadingScreen.prototype = {
 	},
 
 	remove: function() {
-		this.loadingText.kill();
-		this.loadingText.kill();
-		this.gameNameText.kill();
-		this.devText.kill();
-		this.adviceText.kill();
-		this.back.kill();
+		this.hud.destroy();
 
-		this.controlsText.kill();
-		this.jumpText.kill();
-		this.shootText.kill();
 		for (var i = 0; i < this.updateId.length; i++) {
 			clearInterval(this.updateId[i]);
 		};
